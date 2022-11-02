2a745f964f84294127222f779655233e00aba576
Tricky Licky <wangombevictor5@gmail.com>
2022-11-02 21:31:51 +0300

    complete assignment

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+    id INTEGER PRIMARY KEY,
+    name STRING,
+    age INTEGER,
+    sex STRING,
+    color STRING,
+    temperament STRING,
+    alive BOOLEAN
+);
---
+++ lib/insert.sql
@@ -0,0 +1,20 @@
+INSERT INTO bears (id ,name, age , sex , color , temperament , alive) VALUES 
+(1,"Mr. Chocolate",22,"M","green","social",1),
+(2,"Rowdy",22,"M","green","social",0),
+(3,"Tabitha",24,"F","green","social",0),
+(4,"Sergeant Brown",25,"M","green","social",1),
+(5,"Melissa",21,"F","green","social",0),
+(6,"Grinch",23,"M","green","social",1),
+(7,"Wendy",22,"F","green","social",0),
+(8,null,25,"M","green","social",1);
+
+
+
+-- 
+-- 
+-- 
+-- 
+-- 
+-- 
+-- 
+-- unnamed (refer back to how to create a record that doesn't have one value)
